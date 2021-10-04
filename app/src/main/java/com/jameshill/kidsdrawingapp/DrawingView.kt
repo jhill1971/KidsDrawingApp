429af8fcdc42526912a9100a4b61fbe1a05c32d6
jhill1971 <1971jamesh@gmail.com>
2021-10-04 14:16:10 -0400

    October 4 2021 14:15 Drawing persistent. Brush size selector working.

--- app/src/main/java/com/jameshill/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/jameshill/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.jameshill.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -16,6 +17,7 @@ class DrawingView(context: Context,attrs: AttributeSet ): View(context, attrs) {
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
 
     init{
         setUpDrawing()
@@ -30,7 +32,7 @@ class DrawingView(context: Context,attrs: AttributeSet ): View(context, attrs) {
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()//Redundant. Now declared in MainActivity.kt
 
     }
 
@@ -43,6 +45,13 @@ class DrawingView(context: Context,attrs: AttributeSet ): View(context, attrs) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for (path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
         if (!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -73,6 +82,7 @@ class DrawingView(context: Context,attrs: AttributeSet ): View(context, attrs) {
                 }
             }
             MotionEvent.ACTION_UP ->{
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
@@ -81,6 +91,13 @@ class DrawingView(context: Context,attrs: AttributeSet ): View(context, attrs) {
         return true
         
 
+    }
+
+    fun setSizeForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        newSize,resources.displayMetrics )
+
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float ) : Path(){
--- app/src/main/java/com/jameshill/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/jameshill/kidsdrawingapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.jameshill.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.drawing_brush_layout.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawing_view.setSizeForBrush(20.toFloat())
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.drawing_brush_layout)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.ib_small_brush
+        smallBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val medBtn = brushDialog.ib_medium_brush
+        medBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn = brushDialog.ib_large_brush
+        largeBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
